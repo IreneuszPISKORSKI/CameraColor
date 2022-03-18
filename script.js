4dcf503fe42e248d53b78471e73e09f9fec0d7a1
IreneuszPISKORSKI <96122921+IreneuszPISKORSKI@users.noreply.github.com>
2022-03-18 16:56:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 var s = 640; // taille du canvas
 var capture; 
 var slider; // mon slider qui va gérer la quantité de rouge à mettre dans l'image
-
-// function preload() { // fonction qui permet de pré charger l'image
-//   capture = loadImage('images/fractal.jpg');
-// }
+let suma;
 
 function setup(){
   createCanvas(s,480); 
-  slider = createSlider(0, 5, 1); // création du slider avec valeur min, max, et valeur initiale
-  slider.position(10, 10); // position du slider dans la page
-  slider.style('width', '80px'); // largeur du slider (css)
+  slider = createSlider(0, 765, 300); // création du slider avec valeur min, max, et valeur initiale
+  slider.position(700, 100); // position du slider dans la page
+  slider.style('width', '250px'); // largeur du slider (css)
   capture = createCapture(VIDEO);
   capture.size(640, 480);
   pixelDensity(1); // pour ne pas qu'il y ait de bug d'affichage
@@ -18,16 +15,15 @@ function setup(){
 
 function draw(){ //
   
-//   img.resize(s,s); // on déforme l'image pour s'adapter au canvas (on peut faire l'inverse)
-  let facteur =  slider.value(); // récuopération de la valeur du slider
+  let facteur =  slider.value(); 
   loadPixels(); // chargement de la variable "pixels" du canvas
-  capture.loadPixels(); // charge de la variable img.pixels de l'image
+  capture.loadPixels();
   
-  // on boucle sur les lignes et les colonnes de l'image
   for (var ligne = 0; ligne < s; ligne++) { 
     for (var col = 0; col < s; col++) {
         let index = (s * ligne + col)*4;   // correspondance entre la 2D et la 1D 
-        if(((capture.pixels[index + 1])>(capture.pixels[index + 0]))&&((capture.pixels[index + 1])>(capture.pixels[index + 2]))){
+        suma= (capture.pixels[index + 0]+capture.pixels[index + 2]+capture.pixels[index + 1]);
+        if(((capture.pixels[index + 1])>(capture.pixels[index + 0]))&&((capture.pixels[index + 1])>(capture.pixels[index + 2]))&&(suma>facteur)){
           pixels[index + 0] = capture.pixels[index + 0]; 
           pixels[index + 1] = capture.pixels[index + 1]; 
           pixels[index + 2] = capture.pixels[index + 2];  
