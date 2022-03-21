44d0d99df6b79285a5fc9f185aaba2c53a1f5c16
IreneuszPISKORSKI <96122921+IreneuszPISKORSKI@users.noreply.github.com>
2022-03-21 15:36:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ var s = 640; // taille du canvas
 var capture; 
 var slider; // mon slider qui va gérer la quantité de rouge à mettre dans l'image
 let suma;
+let img;
+
+function preload(){
+  img = loadImage("fractal.jpg");
+}
 
 function setup(){
-  createCanvas(s,480); 
+  createCanvas(640,480); 
   slider = createSlider(0, 765, 300); // création du slider avec valeur min, max, et valeur initiale
   slider.position(700, 100); // position du slider dans la page
   slider.style('width', '250px'); // largeur du slider (css)
@@ -18,24 +23,26 @@ function draw(){ //
   let facteur =  slider.value(); 
   loadPixels(); // chargement de la variable "pixels" du canvas
   capture.loadPixels();
-  
+  img.resize(640,480);
+  img.loadPixels();
+  background(0);
   for (var ligne = 0; ligne < s; ligne++) { 
     for (var col = 0; col < s; col++) {
-        let index = (s * ligne + col)*4;   // correspondance entre la 2D et la 1D 
-        suma= (capture.pixels[index + 0]+capture.pixels[index + 2]+capture.pixels[index + 1]);
-        if(((capture.pixels[index + 1])>(capture.pixels[index + 0]))&&((capture.pixels[index + 1])>(capture.pixels[index + 2]))&&(suma>facteur)){
-          pixels[index + 0] = capture.pixels[index + 0]; 
-          pixels[index + 1] = capture.pixels[index + 1]; 
-          pixels[index + 2] = capture.pixels[index + 2];  
-          pixels[index + 3] = 0;
-        }else{
-          pixels[index + 0] = capture.pixels[index + 0]; // on multiplie la composante rouge grâce au slider
-          pixels[index + 1] = capture.pixels[index + 1]; // on recoopie la composante verte de l'image
-          pixels[index + 2] = capture.pixels[index + 2]; // on recoopie la composante bleue de l'image
-          pixels[index + 3] = capture.pixels[index + 3]; // on recoopie la composante alpha de l'image
+      let index = (s * ligne + col)*4;   // correspondance entre la 2D et la 1D 
+      suma= (capture.pixels[index + 0]+capture.pixels[index + 2]+capture.pixels[index + 1]);
+      if(((capture.pixels[index + 1])>(capture.pixels[index + 0]))&&(suma>facteur)){
+        pixels[index + 0] = img.pixels[index + 0];
+        pixels[index + 1] = img.pixels[index + 1];
+        pixels[index + 2] = img.pixels[index + 2];
+        pixels[index + 3] = img.pixels[index + 3];
+      }else{
+        pixels[index + 0] = capture.pixels[index + 0]; // on multiplie la composante rouge grâce au slider
+        pixels[index + 1] = capture.pixels[index + 1]; // on recoopie la composante verte de l'image
+        pixels[index + 2] = capture.pixels[index + 2]; // on recoopie la composante bleue de l'image
+        pixels[index + 3] = capture.pixels[index + 3]; // on recoopie la composante alpha de l'image
       }
     }
   }
-  background(0);
+  
   updatePixels();   // mise à jour de la valeur des pixels du canvas
 }
