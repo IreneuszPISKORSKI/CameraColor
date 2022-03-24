dcf27e844082c2669cf1e920dace2e3cd070ae69
IreneuszPISKORSKI <96122921+IreneuszPISKORSKI@users.noreply.github.com>
2022-03-24 17:01:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var s = 640; // taille du canvas
+var s = 320;
 var capture; 
 var slider; // mon slider qui va gérer la quantité de rouge à mettre dans l'image
-let suma;
+let hueCapt;
+let satCapt;
+let brighCapt;
 let img;
 
 function preload(){
@@ -9,33 +11,39 @@ function preload(){
 }
 
 function setup(){
-  createCanvas(640,480); 
-  slider = createSlider(0, 765, 300); // création du slider avec valeur min, max, et valeur initiale
+  createCanvas(320,240); 
+  frameRate(30); 
+  slider = createSlider(130, 160, 130); // création du slider avec valeur min, max, et valeur initiale
   slider.position(700, 100); // position du slider dans la page
   slider.style('width', '250px'); // largeur du slider (css)
-  capture = createCapture(VIDEO);
-  options = {video: {facingMode: {exact: "environment"}}};
-  capture.size(640, 480);
+  capture = createCapture(VIDEO) ;
+  capture.size(320,240);
+  // capture.hide();
   pixelDensity(1); // pour ne pas qu'il y ait de bug d'affichage
 }
 
-function draw(){ //
+function draw(){ 
   
   let facteur =  slider.value(); 
   loadPixels(); // chargement de la variable "pixels" du canvas
   capture.loadPixels();
-  img.resize(640,480);
+  img.resize(320,240);
   img.loadPixels();
   background(0);
   for (var ligne = 0; ligne < s; ligne++) { 
     for (var col = 0; col < s; col++) {
-      let index = (s * ligne + col)*4;   // correspondance entre la 2D et la 1D 
-      suma= (capture.pixels[index + 0]+capture.pixels[index + 2]+capture.pixels[index + 1]);
-      if(((capture.pixels[index + 1])>(capture.pixels[index + 0]))&&(suma>facteur)){
+      
+      let index = (s * ligne + col)*4;   // correspondance entre la 2D et la 1D
+      let captureColor = color(capture.pixels[index + 0], capture.pixels[index + 1], capture.pixels[index + 2]);
+      
+      hueCapt = hue(captureColor);
+      satCapt = saturation(captureColor);
+      brighCapt = brightness(captureColor);
+
+      if((hueCapt>=100)&&(hueCapt<=180)&&(satCapt>40)&&(brighCapt>25)&&(brighCapt<95)){
         pixels[index + 0] = img.pixels[index + 0];
         pixels[index + 1] = img.pixels[index + 1];
         pixels[index + 2] = img.pixels[index + 2];
-        pixels[index + 3] = img.pixels[index + 3];
       }else{
         pixels[index + 0] = capture.pixels[index + 0]; // on multiplie la composante rouge grâce au slider
         pixels[index + 1] = capture.pixels[index + 1]; // on recoopie la composante verte de l'image
@@ -44,6 +52,5 @@ function draw(){ //
       }
     }
   }
-  
   updatePixels();   // mise à jour de la valeur des pixels du canvas
 }
